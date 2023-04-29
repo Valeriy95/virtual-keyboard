ae9781b6797681dd91abd45c3d1e59e81cf7500a
Valeriy95 <113552340+Valeriy95@users.noreply.github.com>
2023-04-29 15:31:05 +0300

    feat: add styles via JS

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ import {
 
 const keyCodes = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace', 'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete', 'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter', 'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight', 'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'];
 
+function addStyles() {
+  const head = document.querySelector('head');
+  head.insertAdjacentHTML('beforeend', '<link rel="stylesheet" href="normalize.css"><link rel="stylesheet" href="style.css">');
+}
+
+window.addEventListener('load', addStyles);
+
 const body = document.querySelector('body');
 
 const textarea = document.createElement('textarea');
